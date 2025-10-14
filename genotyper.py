e27de33a4e5d1e3b8e9b17a71381beef9c9c08b5
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2025-10-14 07:54:36 -0700

    Add dominance sorting.

--- genotyper.py
+++ genotyper.py
@@ -3,6 +3,8 @@
 inputA = 'AABb'
 inputB = 'aabB'
 
+print('input', inputA, inputB)
+
 def gameteSetsFromChromosome(c):
   if len(c) < 0 or len(c) % 2 != 0:
     raise 'Invalid chromosome length.'
@@ -26,5 +28,18 @@ def zygoteFromGametes(g0, g1):
 def crossGameteSets(s0, s1):
   return [zygoteFromGametes(g0, g1) for g1 in s1 for g0 in s0]
 
-print(crossGameteSets(gameteSetsFromChromosome(inputA),
-                      gameteSetsFromChromosome(inputB)))
+offspring = crossGameteSets(
+  gameteSetsFromChromosome(inputA), gameteSetsFromChromosome(inputB))
+
+def sortGenesDominantFirst(c):
+  if len(c) < 0 or len(c) % 2 != 0:
+    raise 'Invalid chromosome length.'
+  if len(c) == 0:
+    return ''
+
+  a0, a1, tail = c[0], c[1], c[2:]
+  if (a0.isupper()):
+    return a0 + a1 + sortGenesDominantFirst(tail)
+  return a1 + a0 + sortGenesDominantFirst(tail)
+  
+print([sortGenesDominantFirst(c) for c in offspring])
